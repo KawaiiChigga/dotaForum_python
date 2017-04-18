1e9d997f55ca5bc136b29a8997725ec29a679c75
glenntuyu <glenn@tuyu.com>
2017-04-18 23:06:09 +0700

    edit profile

--- dotaForum/dota/views.py
+++ dotaForum/dota/views.py
@@ -78,14 +78,17 @@ def user(request, pk):
         return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
-        serializer = UserSerializer(queryset)
+        serializer = RegSerializer(queryset)
         return Response(serializer.data)
     elif request.method == 'PUT':
-        serializer = UserSerializer(queryset, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            serializer = RegSerializer(queryset, data=request.data, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+        except User.DoesNotExist:
+            return Response(None)
+        # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 # ----------------------CATEGORIES----------------------------
